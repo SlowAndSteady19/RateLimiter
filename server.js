85b43b5d1e6ddd19370747011ec4ce333f8919e8
SlowAndSteady19 <hritujamalgundkar2510@gmail.com>
2025-12-12 10:28:06 +0530

    Fix Vercel homepage

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/admin", adminRoutes);
 
 if (!process.env.VERCEL) {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 export default app;
